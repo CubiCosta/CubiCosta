16e673d68afba26ceca57c6ff70c140fea26fe95
CubiCosta <monkey.d.ruffy.lorenor.zorro@gmail.com>
2026-09-21 17:47:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function showPage(page) {
     else if (page === "gleis") {
 
         content.innerHTML = `
-            <img src="img/Gleis.jpg"></img>
+            <img class="property-images" src="img/Gleis.jpg"></img>
         `;
 
         createPropertyLinkButtons(gleis);
