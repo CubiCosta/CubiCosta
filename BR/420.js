cd064effa481018d18594762ef2e3f65a7a0ca2e
CubiCosta <monkey.d.ruffy.lorenor.zorro@gmail.com>
2026-09-22 14:46:17 +0200

    Update 420.js

--- BR/420.js
+++ BR/420.js
@@ -31,9 +31,50 @@ const Info420 = [
             "img/420IS200.jpg", 
             "img/420IS510.jpg",
             "img/420IS520.jpg",
-            "img/420IS530.jpg",
-            "img/420IS540.jpg"
+            "img/420IS530.jpg"
             ]
+    },
+    {
+        name: "",
+        text: [["", ""]],
+        zeit: [["", ""]],
+        bilder: [""]
+    },
+    {
+        name: "",
+        text: [["", ""]],
+        zeit: [["", ""]],
+        bilder: [""]
+    },
+    {
+        name: "",
+        text: [["", ""]],
+        zeit: [["", ""]],
+        bilder: [""]
+    },
+    {
+        name: "IS540",
+        text: [["", ""]],
+        zeit: [
+            ["AK Führerstand", "180min"],
+            ["AK Fahgastraum", "400min"],
+            ["AK Dach", "240min"],
+            ["AK Außen", "800min"],
+            ["AK Unterfur", "800min"],
+            ["AK Fahrmotor", "600min"],
+            ["AK Filter", "520min"],
+            ["AK Batterie", "120min"],
+            ["AK LZB", "150min"],
+            ["AK Elektrisch", "800min"],
+            ["AK schließen", "180min"],
+            ["AK Türen FST", "90min"],
+            ["AK Türen Fgr. AT", "500min"],
+            ["AK Türen Fgr. MW", "500min"],
+            ["AK Türen Fgr. BT", "500min"],
+            ["AK Türen außen", "200min"],
+            ["AK Video prüfen", "30min"]
+              ],
+        bilder: [""]
     }
     
 ]
