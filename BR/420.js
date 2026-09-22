d715bd34946109139302be95c5eb8627948f2382
CubiCosta <monkey.d.ruffy.lorenor.zorro@gmail.com>
2026-09-22 09:16:43 +0200

    Update 420.js

--- BR/420.js
+++ BR/420.js
@@ -2,15 +2,18 @@ const Info420 = [
     {
         name: "Lagerdeckel",
         text: [
-            ["Wegimpulsgeber PZB", "30Nm"],
+            ["Druckkappe", "200+20Nm"],
             ["Wegimpulsgeber LZB", "151Nm"],
             ["Ferngeber", "20Nm"],
-            ["Lagerdeckel", "100Nm"],
-            ["Schleifring", "28Nm"],
-            ["Schutzhaube", "35Nm"]
+            ["Lagerdeckel", "100+10Nm"],
+            ["Schleifring", "28+1Nm"],
+            ["Schutzhaube", "35Nm"],
+            ["Grundplatte Erdung", "54Nm"],
+            ["Polrad", "75+10Nm"],
+            ["SAP plus Drehmoment", "Kontrolle Bild Unten"
         ],
         zeit: [
-            ["Lagerdeckel...", "0min"]
+            ["Lagerdeckel usw.", "unbekannt"]
         ],
         bilder: [
             "img/deckel420.jpg"
