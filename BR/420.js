2eab2633a91d034e6949990f088a2906eb951ed2
CubiCosta <monkey.d.ruffy.lorenor.zorro@gmail.com>
2026-09-22 16:04:28 +0200

    Update 420.js

--- BR/420.js
+++ BR/420.js
@@ -19,39 +19,99 @@ const Info420 = [
             "img/deckel420.jpg"
         ]
     },
+    
     {
-        name: "IS200 - IS540",
+        name: "IS200",
         text: [
             ["", ""]
             ],
         zeit: [
-            ["siehe bild",""]
+            ["AK30","90min"],
+            ["AK31","90min"],
+            ["AK32","100min"],
+            ["AK33","100min"],
+            ["AK LZB","120min"],
+            ["AK Batterie","90min"],
+            ["AK Video","5min"]
             ],
-        bilder: [
-            "img/420IS200.jpg", 
-            "img/420IS510.jpg",
-            "img/420IS520.jpg",
-            "img/420IS530.jpg"
-            ]
+        bilder: []
     },
+    
     {
-        name: "",
+        name: "IS510",
         text: [["", ""]],
-        zeit: [["", ""]],
-        bilder: [""]
+        zeit: [
+            ["AK Führerstand", "140min"],
+            ["AK Fahgastraum", "140min"],
+            ["AK Dach", "120min"],
+            ["AK Außen", "150min"],
+            ["AK Unterfur", "200min"],
+            ["AK Fahrmotor", "100min"],
+            ["AK Filter", "?min"],
+            ["AK Batterie", "120min"],
+            ["AK LZB", "150min"],
+            ["AK Elektrisch", "200min"],
+            ["AK schließen", "180min"],
+            ["AK Türen FST", "100min"],
+            ["AK Türen Fgr. AT", "200min"],
+            ["AK Türen Fgr. MW", "200min"],
+            ["AK Türen Fgr. BT", "200min"],
+            ["AK Türen außen", "100min"],
+            ["AK Video prüfen", "5min"]
+              ],
+        bilder: []
     },
+    
     {
-        name: "",
+        name: "IS520",
         text: [["", ""]],
-        zeit: [["", ""]],
-        bilder: [""]
+        zeit: [
+            ["AK Führerstand", "180min"],
+            ["AK Fahgastraum", "180min"],
+            ["AK Dach", "200min"],
+            ["AK Außen", "300min"],
+            ["AK Unterfur", "250min"],
+            ["AK Fahrmotor", "180min"],
+            ["AK Filter", "?min"],
+            ["AK Batterie", "120min"],
+            ["AK LZB", "150min"],
+            ["AK Elektrisch", "800min"],
+            ["AK schließen", "180min"],
+            ["AK Türen FST", "90min"],
+            ["AK Türen Fgr. AT", "500min"],
+            ["AK Türen Fgr. MW", "500min"],
+            ["AK Türen Fgr. BT", "500min"],
+            ["AK Türen außen", "200min"],
+            ["AK Video prüfen", "5min"]
+              ],
+        bilder:[]
     },
+    
     {
-        name: "",
+        name: "IS530",
         text: [["", ""]],
-        zeit: [["", ""]],
-        bilder: [""]
+        zeit: [
+            ["AK Führerstand", "300min"],
+            ["AK Fahgastraum", "240min"],
+            ["AK Dach", "300min"],
+            ["AK Außen", "360min"],
+            ["AK Unterfur", "300min"],
+            ["AK Fahrmotor", "300min"],
+            ["AK Filter", "?min"],
+            ["AK Batterie", "120min"],
+            ["AK LZB", "150min"],
+            ["AK Elektrisch", "800min"],
+            ["AK schließen", "180min"],
+            ["AK Türen FST", "100min"],
+            ["AK Türen Fgr. AT", "400min"],
+            ["AK Türen Fgr. MW", "400min"],
+            ["AK Türen Fgr. BT", "400min"],
+            ["AK Türen außen", "180min"],
+            ["AK Video prüfen", "5min"]
+              ],
+        bilder: []
     },
+    
     {
         name: "IS540",
         text: [["", ""]],
@@ -62,19 +122,19 @@ const Info420 = [
             ["AK Außen", "800min"],
             ["AK Unterfur", "800min"],
             ["AK Fahrmotor", "600min"],
-            ["AK Filter", "520min"],
+            ["AK Filter", "?min"],
             ["AK Batterie", "120min"],
             ["AK LZB", "150min"],
             ["AK Elektrisch", "800min"],
             ["AK schließen", "180min"],
-            ["AK Türen FST", "90min"],
+            ["AK Türen FST", "100min"],
             ["AK Türen Fgr. AT", "500min"],
             ["AK Türen Fgr. MW", "500min"],
             ["AK Türen Fgr. BT", "500min"],
             ["AK Türen außen", "200min"],
-            ["AK Video prüfen", "30min"]
+            ["AK Video prüfen", "5min"]
               ],
-        bilder: [""]
+        bilder: []
     }
     
 ]
